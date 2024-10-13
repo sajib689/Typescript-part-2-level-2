d59f4e7598a538b26ce2953509cbdd6ab24b2f15
sajib689 <jibonraja11@gmail.com>
2024-10-13 19:28:20 +0600

    fetch data from the json placehoder

---
+++ src/asynchoronus.ts
@@ -0,0 +1,36 @@
+
+
+const makePromise = () => {
+  return  new Promise<string>((resolve, reject) => {
+        const data:string = 'data fetched from server';
+        if(data) {
+            resolve(data)
+        } else {
+            reject('Failed to fetch data from server')
+        }
+    })
+}
+
+const getPromise = async (): Promise<string> => {
+    const result = await makePromise()
+    return result
+}
+
+const result7 = getPromise()
+interface ITod  {
+    userId: number,
+    id: number,
+    title: string,
+    completed: boolean
+  }
+const getData = async () : Promise<ITod> => {
+    const res = await fetch('https://jsonplaceholder.typicode.com/todos/1')
+    return await res.json()
+}
+
+const getAllData = async () => {
+    const result = await getData()
+    console.log(result)
+    return result
+}
+console.log(getAllData())
--- src/generic-constraints.ts
+++ src/generic-constraints.ts
@@ -1,4 +1,4 @@
-const addMyCrush1 = <T>(myInfo: T) => {
+const addMyCrush1 = <T extends {name: string, age: number, salary: number}>(myInfo: T) => {
     const crush = 'Rupali Khatun'
     const newData = {...myInfo, crush}
     return newData
@@ -7,7 +7,8 @@ const addMyCrush1 = <T>(myInfo: T) => {
 const myInfo1 = {
     name: 'sodor ali',
     age: 34,
-    salary: 45400
+    salary: 45400,
+    other: 'ssfd'
 }
 const result6 = addMyCrush1(myInfo1)
 result6.salary = 2000000
