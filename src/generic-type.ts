4ffb98414a36295224430b74f01f8a6f92e4504f
sajib689 <jibonraja11@gmail.com>
2024-10-13 14:38:00 +0600

    add Tuple Generic system

--- src/generic-type.ts
+++ src/generic-type.ts
@@ -1,2 +1,8 @@
-const rooNumbers: Array<number> = [55, 67, 56]
-const names: Array<string> = ['kuddus', 'rahima']
+type GenericArray<T> = Array<T>
+type GenericTuple<X,Y> = [X,Y]
+const rollNumbers: GenericArray<number> = [55, 67, 56]
+const names: GenericArray<string> = ['kuddus', 'rahima']
+
+const usersDetails: GenericArray<{}> = [{name: 'sajib'}]
+
+console.log(rollNumbers,names,usersDetails)
