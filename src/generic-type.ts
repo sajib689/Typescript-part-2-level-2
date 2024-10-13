99f836aa16cb9dab3aee4eb4cbf7d6d2ad51c7cd
sajib689 <jibonraja11@gmail.com>
2024-10-13 14:45:39 +0600

    GenericTuple

--- src/generic-type.ts
+++ src/generic-type.ts
@@ -1,8 +1,13 @@
 type GenericArray<T> = Array<T>
 type GenericTuple<X,Y> = [X,Y]
+
+const relation : GenericTuple<string,string> = ['sajib', 'Kudds']
+const relationTwo : GenericTuple<string,object> = ['saju', {
+    name: 'sajib',
+}]
 const rollNumbers: GenericArray<number> = [55, 67, 56]
 const names: GenericArray<string> = ['kuddus', 'rahima']
 
 const usersDetails: GenericArray<{}> = [{name: 'sajib'}]
 
-console.log(rollNumbers,names,usersDetails)
+console.log(relationTwo)
