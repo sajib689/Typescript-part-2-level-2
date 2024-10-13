e61a9afbc7aeec0c26a5aa6705d5e3ab27f462b3
sajib689 <jibonraja11@gmail.com>
2024-10-13 15:42:36 +0600

    add spreed operator

--- src/generic-function.ts
+++ src/generic-function.ts
@@ -3,3 +3,17 @@ const createArray1 = <T>(param: T) => {
 }
 
 const result1 = createArray1<string>('Bangladesh')
+
+const addMyCrush = (myInfo: object) => {
+    const crushName = 'Rupali'
+    const newData = {...myInfo, crushName}
+    return newData
+}
+
+const myInfo = {
+    name: 'sajib',
+    age: 33,
+    salary: 4444
+}
+const result5 = addMyCrush(myInfo)
+console.log(result5)
